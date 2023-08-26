6bf5ade6d5fa0f45ecd770ef5aa18a8870d96453
wyeha <snovavzloman@mail.ru>
2023-08-26 14:43:58 +0300

    3rd done

---
+++ Example3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+void FillArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}, ");
+        else if (i > 0 && i < array.Length-1) Console.Write($"{array[i]}, ");
+        else if (i == array.Length - 1) Console.Write($"{array[i]}]");
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
