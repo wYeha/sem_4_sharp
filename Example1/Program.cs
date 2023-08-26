62a0cf7ff256b6149ed29c04933efcfaf1fac104
wyeha <snovavzloman@mail.ru>
2023-08-26 13:38:29 +0300

    1st done

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Алгоритм нужно реализовать самостоятельно, встроенную функцию использовать нельзя.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Power(int number, int power)
+{
+    int result = number;
+    for (int i = 1; i < power; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+Console.WriteLine("Введи число, затем введи степень этого числа, через Enter: ");
+
+Console.WriteLine($"Результат возвездения в степень -> {Power(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()))}");
